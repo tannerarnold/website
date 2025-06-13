c614d82e9fe2a7c4fc50d0cae4b21cf6d7ce4167
Tanner Arnold <tanner.arnold.tech@gmail.com>
2025-06-13 12:38:56 -0600

    add dark mode

---
+++ src/components/dark-mode/DarkModeToggle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { getTheme, setTheme } from '@utils/dark-mode';
+  let currentMode = $state<'light' | 'dark'>(getTheme());
+  function toggleTheme() {
+    if (currentMode === 'light') {
+      currentMode = 'dark';
+      setTheme('dark');
+    } else {
+      currentMode = 'light';
+      setTheme('light');
+    }
+  }
+</script>
+
+<button onclick={toggleTheme}>
+  {#if currentMode === 'light'}
+    Toggle To Dark
+  {:else}
+    Toggle To Light
+  {/if}
+</button>
---
+++ src/utils/dark-mode.ts
@@ -0,0 +1,31 @@
+type Theme = 'light' | 'dark';
+
+const themeContextKey = 'theme';
+const prefersDarkThemeQuery = '(prefers-color-scheme: dark)';
+
+const themes: Record<'light' | 'dark', Theme> = {
+  dark: 'dark',
+  light: 'light',
+};
+
+const prefersDarkTheme = () => window.matchMedia(prefersDarkThemeQuery).matches;
+
+export const getTheme = (): Theme => {
+  const preferredTheme: Theme = prefersDarkTheme() ? themes.dark : themes.light;
+  const currentTheme: Theme =
+    (localStorage.getItem(themeContextKey) as Theme) ?? preferredTheme;
+  if (currentTheme === 'dark') document.body.classList.add('dark');
+  return currentTheme;
+};
+
+export const setTheme = (theme: Theme) => {
+  const currentTheme: Theme = localStorage.getItem(themeContextKey) as Theme;
+  if (currentTheme !== theme) {
+    if (currentTheme === 'dark') {
+      document.body.classList.remove('dark');
+    } else {
+      document.body.classList.add('dark');
+    }
+  }
+  localStorage.setItem(themeContextKey, theme);
+};
