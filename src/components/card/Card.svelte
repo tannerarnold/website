b2148a3bcd353387088ba824fc8b5be5aeaf30eb
Tanner Arnold <tanner.arnold.tech@gmail.com>
2025-06-13 11:25:55 -0600

    Add basic card component

---
+++ src/components/card/Card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cx } from '@utils/class-merge';
+  import type { Snippet } from 'svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  let {
+    children,
+    ...options
+  }: HTMLAttributes<HTMLDivElement> & { children: Snippet | null } = $props();
+</script>
+
+<div {...options} class={cx('card', options.class as string | undefined)}>
+  {@render children?.()}
+</div>
--- src/utils/class-merge.ts
+++ src/utils/class-merge.ts
@@ -2,5 +2,13 @@
  * Merge a list of CSS classes into a single CSS super class list.
  * @param classes
  */
-export const cx = (...classes: string[]) =>
-  classes.reduce((clazz, curr) => (curr + ' ' + clazz).trim(), '');
+export const cx = (...classes: (string | undefined)[]) => {
+  let clazzes = '';
+  for (const clazz of classes) {
+    if (!clazz) continue;
+    else {
+      clazzes = clazzes + ' ' + clazz;
+    }
+  }
+  return clazzes.trim();
+};
