916555aa014402fcd77e54265c6de358051178de
Tanner Arnold <tanner.arnold.tech@gmail.com>
2025-06-13 19:39:02 -0600

    Add card grid

---
+++ src/components/card/CardGrid.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { cx } from '@utils/class-merge';
+  import type { Snippet } from 'svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
+  let {
+    children,
+    ...options
+  }: HTMLAttributes<HTMLDivElement> & { children?: Snippet | null } = $props();
+</script>
+
+<div {...options} class={cx('card-grid', options.class as string)}>
+  {#if children}
+    {@render children?.()}
+  {:else}
+    <p>Nothing to show.</p>
+  {/if}
+</div>
