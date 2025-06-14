5e0a520bf27218ef757acbf8d31de72ab5c680a2
Tanner Arnold <tanner.arnold.tech@gmail.com>
2025-06-13 19:39:26 -0600

    Add card type: hollow vs filled

--- src/components/card/Card.svelte
+++ src/components/card/Card.svelte
@@ -3,14 +3,18 @@
   import type { Snippet } from 'svelte';
   import type { HTMLAttributes } from 'svelte/elements';
 
+  type CardType = 'hollow' | 'filled';
+
   let {
     children,
+    type = 'hollow',
     ...options
   }: HTMLAttributes<HTMLDivElement> & {
+    type?: CardType;
     children: Snippet | null;
   } = $props();
 </script>
 
-<div {...options} class={cx('card', options.class as string | undefined)}>
+<div {...options} class={cx('card', type, options.class as string | undefined)}>
   {@render children?.()}
 </div>
